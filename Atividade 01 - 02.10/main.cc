efaeff92e12ac5eef31c001b270f2c550e3b3568
Arthur Felipe <arthur.felipe.00@outlook.com>
2023-10-03 12:42:29 -0300

    Criação da terceira e última imagem: um triângulo azul

--- Atividade 01 - 02.10/main.cc
+++ Atividade 01 - 02.10/main.cc
@@ -7,16 +7,15 @@ int main() {
     const int image_width = 256;
     const int image_height = 256;
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-    // Render (Degradê)
+    // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+    // Imagem Degradê
+
     for (int j = 0; j < image_height; ++j) {
-
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-
         for (int i = 0; i < image_width; ++i) {
             int red = 0;
             int green = j;
@@ -28,19 +27,14 @@ int main() {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-    // Render (Rosquinha)
+    // Imagem Rosquinha
 
     const int centerX = image_width / 2;
     const int centerY = image_height / 2;
     const int outerRadius = 90;
     const int innerRadius = 60;
 
-    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
     for (int j = 0; j < image_height; ++j) {
-
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-
         for (int i = 0; i < image_width; ++i) {
             int dx = i - centerX;
             int dy = j - centerY;
@@ -51,9 +45,7 @@ int main() {
             int blue = 0;
 
             if (distSquared <= outerRadius * outerRadius && distSquared >= innerRadius * innerRadius) {
-                red = 255;
-                green = 0;
-                blue = 0;
+                green = 255;
             }
 
             std::cout << red << ' ' << green << ' ' << blue << '\n';
@@ -62,6 +54,31 @@ int main() {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-    std::clog << "\rDone.                 \n";
+    // Imagem Triângulo
 
+    // Coordenadas dos vértices do triângulo
+    int x1 = image_width / 2;
+    int y1 = image_height / 4;
+
+    int x2 = image_width / 4;
+    int y2 = (3 * image_height) / 4;
+
+    int x3 = (3 * image_width) / 4;
+    int y3 = (3 * image_height) / 4;
+
+    for (int j = 0; j < image_height; ++j) {
+        for (int i = 0; i < image_width; ++i) {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            if (((i - x1) * (y2 - y1) - (j - y1) * (x2 - x1) >= 0) &&
+                ((i - x2) * (y3 - y2) - (j - y2) * (x3 - x2) >= 0) &&
+                ((i - x3) * (y1 - y3) - (j - y3) * (x1 - x3) >= 0)) {
+                blue = 255;
+            }
+
+            std::cout << red << ' ' << green << ' ' << blue << '\n';
+        }
+    }
 }
