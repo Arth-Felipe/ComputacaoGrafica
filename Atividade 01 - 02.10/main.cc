796ff9adb0c26a539759747c9d8a5194eeeb5726
Arthur Felipe <arthur.felipe.00@outlook.com>
2023-10-03 11:44:18 -0300

    Criação de 2 imagens: um degradê e um anel vermelho

--- Atividade 01 - 02.10/main.cc
+++ Atividade 01 - 02.10/main.cc
@@ -4,13 +4,13 @@ int main() {
 
     // Image
 
-    int image_width = 256;
-    int image_height = 256;
+    const int image_width = 256;
+    const int image_height = 256;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
     // Render (Degradê)
-/*
+
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
@@ -18,7 +18,6 @@ int main() {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 
         for (int i = 0; i < image_width; ++i) {
-
             int red = 0;
             int green = j;
             int blue = 255 - j;
@@ -26,44 +25,43 @@ int main() {
             std::cout << red << ' ' << green << ' ' << blue << '\n';
         }
     }
-    std::clog << "\rDone.                 \n";
-*/
+
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-    // Render (Círculo)
+    // Render (Rosquinha)
 
-    int centerX = image_width / 2;
-    int centerY = image_height / 2;
-    int radius = 100;
+    const int centerX = image_width / 2;
+    const int centerY = image_height / 2;
+    const int outerRadius = 90;
+    const int innerRadius = 60;
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; ++j) {
+
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
         for (int i = 0; i < image_width; ++i) {
             int dx = i - centerX;
             int dy = j - centerY;
-            int distanceSquared = dx * dx + dy * dy;
+            int distSquared = dx * dx + dy * dy;
 
-            if (distanceSquared <= radius * radius) {
-                int red = 255;
-                int green = 0;
-                int blue = 0;
+            int red = 0;
+            int green = 0;
+            int blue = 0;
 
-                std::cout << red << " " << green << " " << blue << " ";
-
-            } else {
-                int red = 0;
-                int green = 0;
-                int blue = 0;
-
-                std::cout << red << " " << green << " " << blue << " ";
+            if (distSquared <= outerRadius * outerRadius && distSquared >= innerRadius * innerRadius) {
+                red = 255;
+                green = 0;
+                blue = 0;
             }
+
+            std::cout << red << ' ' << green << ' ' << blue << '\n';
         }
-        std::cout << "\n";
     }
 
-    std::clog << "\rDone.                 \n";
-
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+    std::clog << "\rDone.                 \n";
 
 }
