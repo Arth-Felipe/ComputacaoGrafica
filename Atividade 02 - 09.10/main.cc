f27ff538cff288ab21b6c9057d1bdd1cd3d28c0c
Arthur Felipe <arthur.felipe.00@outlook.com>
2023-10-15 13:12:39 -0300

    Seguindo o tutorial até a seção 3

---
+++ Atividade 02 - 09.10/main.cc
@@ -0,0 +1,37 @@
+/**
+ * @file main.cc
+ * @author Peter Shirley, Trevor David Black, Steve Hollasch
+ * @brief Now we can change our main to use both of these. Final code for the first PPM image
+ * @version 1.0
+ * @date 2023-10-15
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "color.h"
+#include "vec3.h"
+
+#include <iostream>
+
+int main() {
+
+    // Image
+
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+}
